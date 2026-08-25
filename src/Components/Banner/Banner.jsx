1ec4fc59a62c4be176dfa69ca85620c76199df3a
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-08-25 22:13:39 +0300

    feat: add dynamic TMDB banner and secure API key

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,20 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NetflixBannerLogo from '../../assets/image/logo.png'
 import {Play, Info} from 'lucide-react'
 import styles from './Banner.module.css'
+import {movieInstance} from '../../Utility/MovieInstance'
+import requests from '../../Utility/requestUrls'
+
+const BANNER_BASE= "https://image.tmdb.org/t/p/original/"
 
 function Banner() {
+
+    const [bannerImage, setBannerImage] = useState({});
+
+    useEffect(()=>{
+        async function fetchBannerImage() {
+            const request = await movieInstance.get(requests.fetchNetflixOriginals)
+
+            setBannerImage(
+                request.data.results[Math.floor(Math.random()*request.data.results.length)]
+            )
+        }
+        fetchBannerImage()
+    },[])
+
+    function truncate(str,n) {
+        return str?.length>n ?  str.substr(0,n-1) + "..." : str;
+    }
+
     return (
-        <div className={styles.banner}>
+        <div className={styles.banner}
+
+        style={
+            {
+                backgroundSize: "cover",
+                backgroundImage: `url("${BANNER_BASE}${bannerImage.backdrop_path}")`
+            }
+        }
+        >
         <div className={styles.contents}>
             {/* netflix image  */}
             <img className={styles.logoImg} src={NetflixBannerLogo} alt="Netflix logo" />
 
             {/* title  */}
-            <h1 className={styles.title}>Bridgerton</h1>
+            <h1 className={styles.title}>{bannerImage?.original_name}</h1>
 
             {/* description */}
-            <h1 className={styles.description}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit voluptatum facilis necessitatibus eius, voluptatibus non.</h1>
+            <h1 className={styles.description}>
+                {truncate(bannerImage?.overview,120)}
+            </h1>
 
             {/* button  */}
             <div className={styles.buttonContainer}>
@@ -38,6 +70,5 @@ function Banner() {
         </div>
     )
 }
-import { Form } from 'react-router-dom'
 
 export default Banner
--- src/Utility/requestUrls.js
+++ src/Utility/requestUrls.js
@@ -1,4 +1,4 @@
-const API_KEY = "205c3e5733f30a5bf55667e1da0024d7";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
 const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
