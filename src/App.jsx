053832312b4e6e94bd4381c5ae7441b7168e5ce8
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-08-26 08:23:45 +0300

    feat: add responsive streaming-platform-clone footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Components/Header/Header'
 import Banner from './Components/Banner/Banner'
 import DisplayRow from './Components/DisplayRow/DisplayRow'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <Header />
     <Banner />
     <DisplayRow />
+    <Footer />
     </>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,96 @@
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaYoutube,
+} from "react-icons/fa";
+
+import styles from "./Footer.module.css";
+
+const footerLinks = [
+    [
+        "Audio Description",
+        "Investor Relations",
+        "Legal Notices",
+    ],
+    [
+        "Help Centre",
+        "Jobs",
+        "Cookie Preferences",
+    ],
+    [
+        "Gift Cards",
+        "Terms of Use",
+        "Corporate Information",
+    ],
+    [
+        "Media Centre",
+        "Privacy",
+        "Contact Us",
+    ],
+];
+
+const socialLinks = [
+    {
+        name: "Facebook",
+        icon: <FaFacebookF />,
+        url: "#",
+    },
+    {
+        name: "Instagram",
+        icon: <FaInstagram />,
+        url: "#",
+    },
+    {
+        name: "Twitter",
+        icon: <FaTwitter />,
+        url: "#",
+    },
+    {
+        name: "YouTube",
+        icon: <FaYoutube />,
+        url: "#",
+    },
+];
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.container}>
+                {/* Social Media */}
+                <div className={styles.socials}>
+                    {socialLinks.map((social) => (
+                        <a
+                            key={social.name}
+                            href={social.url}
+                            aria-label={social.name}
+                            className={styles.socialLink}
+                        >
+                            {social.icon}
+                        </a>
+                    ))}
+                </div>
+
+                {/* Footer Links */}
+                <div className={styles.linksGrid}>
+                    {footerLinks.map((column, columnIndex) => (
+                        <ul key={columnIndex} className={styles.linkColumn}>
+                            {column.map((link) => (
+                                <li key={link}>
+                                    <a href="#">{link}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    ))}
+                </div>
+
+                {/* Copyright */}
+                <p className={styles.copyright}>
+                    © 1997-2026 Netflix, Inc.
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
