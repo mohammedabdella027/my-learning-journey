172b88316615d2165f73a1606af5809052df6ec6
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-09-01 16:34:31 +0300

    feat: add backend server entry point and database connection setup

---
+++ 01. AI Full-Stack Web Developer Course/02. AI-Powered Application Architecture & Building the Chat Bot/01. GPT-Clone/Backend/index.js
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import express from 'express';
+import db from './db.config.js';
+
+const app = express();
+
+async function startServer () {
+    try {
+        const connection = await db.getConnection();
+        connection.release();
+        console.log('db connected')
+
+        app.listen(3000, err => {
+            if (err) {
+                throw err
+            }
+
+            console.log("server is running on port http://localhost:3000")
+        });
+    } catch (err) {
+        console.error('error starting server: ', err.message)
+    }
+}
+
+startServer();
