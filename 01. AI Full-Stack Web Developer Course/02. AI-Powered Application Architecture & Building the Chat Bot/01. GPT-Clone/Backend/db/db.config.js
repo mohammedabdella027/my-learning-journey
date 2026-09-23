47ea3ce6f970f82fb7c2d23d00bc83feff382d17
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-09-23 20:56:18 +0300

    configure production database connection

--- 01. AI Full-Stack Web Developer Course/02. AI-Powered Application Architecture & Building the Chat Bot/01. GPT-Clone/Backend/db/db.config.js
+++ 01. AI Full-Stack Web Developer Course/02. AI-Powered Application Architecture & Building the Chat Bot/01. GPT-Clone/Backend/db/db.config.js
@@ -1,10 +1,14 @@
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-})
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 export default db;
