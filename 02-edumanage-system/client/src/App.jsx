5a50c50811e5ed0e8dff881420a23047aa54717c
mohammedabdella027 <mohammedabdella027@gmail.com>
2026-09-26 19:25:53 +0300

    feat: initialize EduManage frontend with Vite, React, and Tailwind CSS v4

---
+++ 02-edumanage-system/client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-indigo-600 pl-8">
+        EduManage System is Ready!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
